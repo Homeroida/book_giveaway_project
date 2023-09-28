a006efcce8761e86747efc4872929aaa547c3d78
Zurab <chanishvili@gmail.com>
2023-09-28 16:18:24 +0400

    add swagger redoc

--- book_giveaway_project/settings.py
+++ book_giveaway_project/settings.py
@@ -48,12 +48,6 @@ INSTALLED_APPS = [
 ]
 
 
-# REST_FRAMEWORK = {
-
-#     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-#     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-# }
-
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Book Giveaway API',
 }
--- book_giveaway_project/urls.py
+++ book_giveaway_project/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import RegistrationView, CurrentUserView
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 from rest_framework.authtoken.views import obtain_auth_token
 
 
@@ -28,6 +28,7 @@ urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('schema/docs/', SpectacularSwaggerView.as_view(url_name='schema')),
+    path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('me/', CurrentUserView.as_view(), name='current-user'),
 
 
